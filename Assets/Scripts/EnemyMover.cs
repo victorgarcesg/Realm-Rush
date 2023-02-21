e8c63c7ed95271571afcb6a02d7a2e4f9f7fc35b
vgarces <victorgarcesgg@gmail.com>
2023-02-21 19:08:44 -0400

    Add enemy movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     private void Start()
     {
@@ -17,9 +17,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (var waypoint in path)
         {
-            var position = waypoint.transform.position;
-            transform.position = new Vector3(position.x, transform.position.y, position.z);
-            yield return new WaitForSeconds(waitTime);
+            var startPosition = transform.position;
+            var endPosition = waypoint.transform.position;
+            transform.LookAt(endPosition);
+
+            float travelPercent = 0f;
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
