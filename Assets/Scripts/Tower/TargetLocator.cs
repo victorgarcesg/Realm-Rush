b9f4216dfd5ab59484faa25fccc4e70aff9d05c0
vgarces <victorgarcesgg@gmail.com>
2023-02-22 16:15:07 -0400

    Look at the closest enemy

--- Assets/Scripts/Tower/TargetLocator.cs
+++ Assets/Scripts/Tower/TargetLocator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,54 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Update()
     {
-        target = FindObjectOfType<EnemyMover>().transform;
+        FindClosestEnemy();
+        AimWeapon();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FindClosestEnemy()
     {
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            var enemy = enemies[i];
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
+    private void AimWeapon()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+        bool canAttack = targetDistance <= range;
+        Attack(canAttack);
         weapon.LookAt(target);
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
  
