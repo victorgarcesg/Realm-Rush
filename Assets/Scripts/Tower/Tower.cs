25e52a8a7c07468aaf61b9a426c3a1b434964e81
vgarces <victorgarcesgg@gmail.com>
2023-02-28 23:25:05 -0400

    Add build timer

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,11 +1,41 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float buildTimer = 1f;
 
     Bank bank;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
+
+    private IEnumerator Build()
+    {
+        // var children = transform.GetComponentsInChildren<Transform>();
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(false);
+            }
+        }
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildTimer);
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(true);
+            }
+        }
+    }
 
     public bool CreateTower(Tower towerPrefab, Vector3 position)
     {
